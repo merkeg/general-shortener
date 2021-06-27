093f60b87f92bd8a00b02f38ab80feb69cff4f33
Egor Merk <egor@merkeg.de>
2021-06-27 15:09:39 +0200

    Added possibility to authenticate via token

--- src/lib/Authentication.ts
+++ src/lib/Authentication.ts
@@ -2,42 +2,46 @@ import express from "express";
 import { AuthenticationError } from "./ErrorHandler";
 
 export async function expressAuthentication(request: express.Request, securityName: string, scopes: string[]): Promise<any> {
-	if (request.cookies.id != undefined) {
-		return handleAuthCookie(request, securityName, scopes);
-	}
+    if (request.body.password != undefined) {
+        return handleAuthBody(request, securityName, scopes);
+    }
 
-	if (request.body.password != undefined) {
-		return handleAuthBody(request, securityName, scopes);
-	}
+    if (request.query.password != undefined) {
+        return handleAuthQuery(request, securityName, scopes);
+    }
 
-	if (request.query.password != undefined) {
-		return handleAuthQuery(request, securityName, scopes);
-	}
+    if (request.headers.authorization != undefined) {
+        return handleAuthToken(request, securityName, scopes);
+    }
 
-	return Promise.reject(new AuthenticationError("Not authenticated"));
-}
-
-async function handleAuthCookie(request: express.Request, securityName: string, scopes: string[]) {
-	var id = request.cookies.id;
-	return Promise.reject(new AuthenticationError("Not yet implemented"));
+    return Promise.reject(new AuthenticationError("Not authenticated"));
 }
 
 async function handleAuthQuery(request: express.Request, securityName: string, scopes: string[]) {
-	if (request.query.password != undefined) {
-		if (request.query.password == process.env.AUTHENTICATION_PASSWORD) {
-			return Promise.resolve(true);
-		}
-		return Promise.reject(new AuthenticationError("Invalid Password"));
-	}
-	return Promise.reject(new AuthenticationError("Password not present"));
+    if (request.query.password != undefined) {
+        if (request.query.password == process.env.AUTHENTICATION_PASSWORD) {
+            return Promise.resolve(true);
+        }
+        return Promise.reject(new AuthenticationError("Invalid Token"));
+    }
+    return Promise.reject(new AuthenticationError("Token not present"));
 }
 
 async function handleAuthBody(request: express.Request, securityName: string, scopes: string[]) {
-	if (request.body.password != undefined) {
-		if (request.body.password == process.env.AUTHENTICATION_PASSWORD) {
-			return Promise.resolve(true);
-		}
-		return Promise.reject(new AuthenticationError("Invalid Password"));
-	}
-	return Promise.reject(new AuthenticationError("Password not present"));
+    if (request.body.password != undefined) {
+        if (request.body.password == process.env.AUTHENTICATION_PASSWORD) {
+            return Promise.resolve(true);
+        }
+        return Promise.reject(new AuthenticationError("Invalid Token"));
+    }
+    return Promise.reject(new AuthenticationError("Token not present"));
+}
+
+async function handleAuthToken(request: express.Request, securityName: string, scopes: string[]) {
+    const bearer = request.headers.authorization.split(" ");
+    const bearerToken = bearer[1];
+    if (bearerToken == process.env.AUTHENTICATION_PASSWORD) {
+        return Promise.resolve(true);
+    }
+    return Promise.reject(new AuthenticationError("Invalid Token"));
 }
