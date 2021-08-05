bc2c1c297a983b24d5da85e7f504b73b3de137a7
Egor Merk <egor@merkeg.de>
2021-08-05 22:09:44 +0200

    Test commit

--- general-shortener/Utils/MarkdownUtils.cs
+++ general-shortener/Utils/MarkdownUtils.cs
@@ -1,14 +1,14 @@
-﻿using Markdig;
+﻿
 using Microsoft.AspNetCore.Html;
 
 namespace general_shortener.Utils
 {
     public class MarkdownUtils
     {
-        /// <summary>
-        /// Markdown pipeline
-        /// </summary>
-        public static MarkdownPipeline Pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().UseEmojiAndSmiley().UseSmartyPants().Build();
+        // /// <summary>
+        // /// Markdown pipeline
+        // /// </summary>
+        // public static MarkdownPipeline Pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().UseEmojiAndSmiley().UseSmartyPants().Build();
 
         /// <summary>
         /// Parse markdown string to html string
@@ -17,7 +17,8 @@ namespace general_shortener.Utils
         /// <returns></returns>
         public static HtmlString ParseHtmlString(string markdown)
         {
-            return new HtmlString(Markdown.ToHtml(markdown, Pipeline));
+            // return new HtmlString(Markdown.ToHtml(markdown, Pipeline));
+            return new HtmlString("<a>This is a test</a>");
         }
     }
 }
